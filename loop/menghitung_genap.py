56b6adad033aea37ae2449f7144fd35a70a2472d
Muhammadridwanprasety0 <142766692+Muhammadridwanprasety0@users.noreply.github.com>
2023-09-04 11:42:04 +0700

    Add files via upload

---
+++ loop/menghitung_genap.py
@@ -0,0 +1,9 @@
+batas + int(input("masukkan batas angka: "))
+jumlah_angka = 0
+
+for angka in range (1, batas+1):
+    if angka % 2 == 0:
+        jumlah genap += 1
+
+ 
+print("jumlah angka genap dalam rentang 1 hingga", batas, "adalah: ", jumlah_genap )
